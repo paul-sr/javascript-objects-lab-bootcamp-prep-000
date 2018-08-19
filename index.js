a2c3d80a80aad67c620c29738125243e69cc4c65
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-19 21:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object
+  var newObj = object
+  newObj[key] = value;
+  return newObj
 }
