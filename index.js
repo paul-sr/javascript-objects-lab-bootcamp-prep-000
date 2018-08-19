0b20d666376ab90d8a1e0da1c25349f9e9ef1386
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-19 21:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  
+  return delete object.key
 }
